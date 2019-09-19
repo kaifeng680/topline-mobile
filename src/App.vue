dd6f8fe3c4a97b4c644f135b141ee8579c7863ec
kaifeng <kaifeng@qq.com>
2019-09-19 10:22:51 +0800

    调整目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div id="app">
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
 
-<style></style>
+}
+</script>
+
+<style>
+
+</style>
