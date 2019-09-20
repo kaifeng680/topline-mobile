6f88b79f056cc5f4c464c8a9b36b89bd5f46d690
kaifeng <kaifeng@qq.com>
2019-09-21 01:20:28 +0800

    首页文章列表真实数据请求完成

--- src/api/article.js
+++ src/api/article.js
@@ -1,3 +1,21 @@
+import request from '@/utils/request'
 /**
- * 文章相关请求模块
+ * 获取文章相关请求模块
  */
+export const getArticles = ({
+  // JavaScript Standard Style 代码风格不允许有非驼峰名命名法的变量
+  // 但是对象的 key 是不检查的
+  channelId,
+  timestamp,
+  withTop
+}) => {
+  return request({
+    method: 'get',
+    url: '/app/v1_1/articles',
+    params: {
+      channel_id: channelId, // 频道的id
+      timestamp, // 时间戳,请求新的推荐数据传当前的时间戳，请求历史推荐传指定的时间戳
+      with_top: withTop // 是否包含置顶，进入页面第一次请求时要包含置顶文章，1-包含置顶，0-不包含
+    }
+  })
+}
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -10,15 +10,15 @@
     :title="channel.name">
      <!-- 文章列表 -->
   <van-list
-  v-model="loading"
-  :finished="finished"
+  v-model="channel.loading"
+  :finished="channel.finished"
   finished-text="没有更多了"
   @load="onLoad"
 >
   <van-cell
-    v-for="item in channel.articles"
-    :key="item"
-    :title="item"
+    v-for="article in channel.articles"
+    :key="article.art_id.toString()"
+    :title="article.title"
   />
 </van-list>
 </van-tab>
@@ -29,21 +29,20 @@
 
 <script>
 import { getAllChannels } from '@/api/channel'
+import { getArticles } from '@/api/article'
 export default {
   name: 'HomeIndex',
   data () {
     return {
-      active: 2, // 控制当前激活的标签页
-      channels: [], // 频道列表
-      list: [],
-      loading: false,
-      finished: false
+      active: 0, // 控制当前激活的标签页
+      channels: [] // 频道列表
     }
   },
   // 计算属性
   computed: {
   //  获取到文章列表的active
     currentChannel () {
+      // active 是动态的，active 改变也就意味着 currentChannel 也改变了
       return this.channels[this.active]
     }
   },
@@ -55,30 +54,59 @@ export default {
     // 获得频道列表
     async loadAllChannels () {
       const { data } = await getAllChannels()
-      // 为每个频道初始化一个成员,articles 用来储存该频道的文章列表
+      //  定制频道的内容数据
       data.data.channels.forEach(channel => {
-        channel.articles = []
+        channel.articles = [] // 当前频道的文章列表
+        channel.loading = false // 频道的上拉加载更多的loading 的状态
+        channel.finished = false //  频道的加载结束的状态
+        channel.timestamp = null // 用于获取下一页的时间戳
       })
       this.channels = data.data.channels
     },
-    onLoad () {
-      // 异步更新数据
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          // 将数据添加到当前频道的,文章列表中
-          // this.list.push(this.list.length + 1);
-          const articles = this.currentChannel.articles
-          articles.push(articles.length + 1)
-        }
-        // 加载状态结束
-        this.loading = false
+    async onLoad () {
+    // 1. 请求加载文章列表
+      const currentChannel = this.currentChannel
+      const { data } = await getArticles({
+        channelId: currentChannel.id,
+        // 第一页传递最新的时间戳
+        // 下一页数据传递上一页返回数据结果中的 pre_timestamp
+        timestamp: currentChannel.timestamp || Date.now(),
+        withTop: 1
+      })
 
-        // 数据全部加载完成
-        if (this.list.length >= 20) {
-          this.finished = true
-        }
-      }, 500)
+      const { pre_timestamp: preTimestamp, results } = data.data
+
+      // 2. 将得到的文章列表添加到当前的频道中 articles 中
+      currentChannel.articles.push(...results)
+
+      //  3. 本次的 onLoad 数据加载完毕,
+      currentChannel.loading = false
+
+      // 4. 判断数据是否还有下一次数据
+      if (!preTimestamp) {
+        currentChannel.finished = true
+      } else {
+        // 还有数据，将本次得到的 preTimestamp 存储到当前频道，用于加载下一页数据
+        currentChannel.timestamp = preTimestamp
+      }
     }
+
+    // onLoad () {
+    //   // 异步更新数据
+    //   setTimeout(() => {
+    //     for (let i = 0; i < 10; i++) {
+    //       // 将数据添加到当前频道的,文章列表中
+    //       const articles = this.currentChannel.articles
+    //       articles.push(articles.length + 1)
+    //     }
+    //     // 加载状态结束
+    //     this.currentChannel.loading = false
+    //     // 数据全部加载完成
+    //     if (this.currentChannel.articles.length >= 20) {
+    //       this.currentChannel.finished = true
+    //     }
+    //   }, 2000)
+    // }
   }
 
 }
